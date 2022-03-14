9645fa17d3b8970756c4428a1f2640325eac1a56
natsuto-kun <natsuto391@gmail.com>
2022-03-14 15:49:29 +0900

    add: sample-page

---
+++ pages/Bad.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+
+export const BadEnd = () => {
+    return (
+        <div>
+            <p>Bad End</p>
+        </div>
+    )
+}
---
+++ pages/True.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+import { css } from '@emotion/css'; 
+
+const sampleText = css`
+  font-size: 22px;
+`;
+
+export const TrueEnd = () => {
+    return (
+        <div>
+            <p className="sampleText">True End</p>
+        </div>
+    )
+}
