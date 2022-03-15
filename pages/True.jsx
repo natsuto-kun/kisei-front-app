62b90aa1b0ddc9f22d2d9c696983de8fe10f850f
banana5237 <hikarinakashima8@gmail.com>
2022-03-16 01:29:37 +0900

    add image,edit Time,Name

--- pages/True.jsx
+++ pages/True.jsx
@@ -2,28 +2,70 @@ import react from "react";
 import { css } from '@emotion/css'; 
 import styled from 'styled-components'
 import Image from 'next/image'
-// import './pages/true.css'
 
 const Time = styled.h1({
+    position:"absolute",
+    left: "203px",
+    top: "100px",
     fontSize: "240px",
-    textAlign: "center",
-    color: "palevioletred",
+    fontFamily:"CoppepanJamAl",
+    color: "#333333",
+    zIndex:5,
 })
-  
+
+const Name =styled.p({
+    position:"absolute",
+    left: "203px",
+    top: "150px",
+    fontSize:"64px",
+    fontFamily:"Agency FB",
+    color:"#333333",
+    zIndex:5,
+})
 const Wrapper = styled.section({
     // padding: "4em",
   });
 
+const bgimage =css`
+    position:"absolute";
+    top:0;
+    left:0;
+    zIndex:0;
+`;
+
+const house = css`
+    position:"absolute";
+    top:0px;
+    left:0;
+    zIndex:3;
+`;
+
+
 
 const TrueEnd = () => {
     return (
-        <div>
-            <Wrapper>
+        <Wrapper>
             <Time>
                 10:00
             </Time>
+                
+            <Name>
+                username
+            </Name>
+
+            <Image className={bgimage}
+            src="/background_true.png"
+            width={1366}
+            height={768}
+            />
+            
+            <Image className={house}
+            src="/house_true.png"
+            width={400}
+            height={500}
+            />
         </Wrapper>
-        </div>
+
     )
 }
 
