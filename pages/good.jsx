8f8d4a1ab8c8286a402502367a92e868754a8665
banana5237 <hikarinakashima8@gmail.com>
2022-03-16 01:59:41 +0900

    add Time,Name,background-image

--- pages/good.jsx
+++ pages/good.jsx
@@ -1,9 +1,52 @@
 import react from "react";
+import styled from 'styled-components'
+import Image from 'next/image'
+import { css } from '@emotion/css'; 
+
+const Time = styled.h1({
+    position:"absolute",
+    left: "203px",
+    top: "100px",
+    fontSize: "240px",
+    fontFamily:"FlamingoSerif",
+    color: "#333333",
+    zIndex:5,
+})
+
+const Name =styled.p({
+    position:"absolute",
+    left: "203px",
+    top: "150px",
+    fontSize:"64px",
+    fontFamily:"Agency FB",
+    color:"#333333",
+    zIndex:5,
+})
+
+const bgimage =css`
+    position:"absolute";
+    top:0;
+    left:0;
+    zIndex:0;
+`;
 
 const GoodEnd = () => {
     return (
         <div>
-            <p>Good End</p>
+            <Time>
+                20:00
+            </Time>
+
+            <Name>
+                username
+            </Name>
+
+            <Image className={bgimage}
+            src="/background_good.png"
+            width={1366}
+            height={768}
+            alt="bgimage"
+            />
         </div>
     )
 }
