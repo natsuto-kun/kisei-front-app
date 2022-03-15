eb61d3b9467e2916f53d4d2603bd7184b17851b3
natsuto-kun <natsuto391@gmail.com>
2022-03-16 00:51:01 +0900

    fix: envの設定

--- components/data/databaseContext.js
+++ components/data/databaseContext.js
@@ -1,17 +1,16 @@
-const config = require("../config");
 const CosmosClient = require("@azure/cosmos").CosmosClient;
 
 /*
 // This script ensures that the database is setup and populated correctly
 */
-async function create(client, databaseId, containerId) {
-  const partitionKey = config.partitionKey;
+async function create(client, DATABASEID, CONTAINERID) {
+  const partitionKey = { kind: process.env.PARTITIONKEY_KIND, paths: process.env.PARTITIONKEY_PATHS };
 
   /**
    * Create the database if it does not exist
    */
   const { database } = await client.databases.createIfNotExists({
-    id: databaseId
+    id: DATABASEID
   });
   console.log(`Created database:\n${database.id}\n`);
 
@@ -19,9 +18,9 @@ async function create(client, databaseId, containerId) {
    * Create the container if it does not exist
    */
   const { container } = await client
-    .database(databaseId)
+    .database(DATABASEID)
     .containers.createIfNotExists(
-      { id: containerId, partitionKey },
+      { id: CONTAINERID, partitionKey },
       { offerThroughput: 400 }
     );
 
