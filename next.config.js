0e2967fee9c7ae57d79498f52cf34668c947057b
natsuto-kun <natsuto391@gmail.com>
2022-03-15 17:09:08 +0900

    fix: next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
+const config = require('./components/config')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   future: {
     webpack5: true,
+  },
+  webpack: (config, {isServer}) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+      config.resolve.fallback.child_process = false;
+      config.resolve.fallback.net = false;
+      config.resolve.fallback.dns = false;
+      config.resolve.fallback.tls = false;
+    }
+    return config;
   }
 }
 
